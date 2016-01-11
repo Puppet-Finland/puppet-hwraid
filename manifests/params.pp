34d3f8b1a6bca8edb1ecec6fe7381efe5d62a51d
Samuli Seppänen <samuli@openvpn.net>
2016-01-11 13:09:58 +0000

    Add support for monitoring 3Ware Eskaled 9000 controllers using 3ware-statusd
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,6 +9,9 @@ class hwraid::params {
         'Debian': {
             $aacraid_package_name = 'aacraid-status'
             $aacraid_service_name = 'aacraid-statusd'
+            $twcli_package_name = 'tw-cli'
+            $ware_status_package_name = '3ware-status'
+            $ware_status_service_name = '3ware-statusd'
         }
         default: {
             fail("Unsupported OS: ${::osfamily}")
---
+++ manifests/ware.pp
@@ -0,0 +1,72 @@
+#
+# == Class: hwraid::ware
+#
+# Configure monitoring for 3Ware Eskaled 9000 series hardware RAID devices. The 
+# name of this class is silly, because Puppet does not allow numbers in the 
+# namespaces (classes, defines, variables, etc).
+#
+# == Parameters
+#
+# [*ensure*]
+#   Status of RAID monitoring. Valid values are 'present' (default) and 
+#   'absent'.
+# [*remind*]
+#   How often to send reminder emails (of degraded arrays). Value is given in 
+#   seconds and defaults to 86400 (24 hours)
+# [*email*]
+#   Email address where notification emails are sent. Defaults to top-scope 
+#   variable $::servermonitor.
+#
+class hwraid::ware
+(
+    $ensure = 'present',
+    $remind = 86400,
+    $email = $::servermonitor
+
+) inherits hwraid::params
+{
+
+    validate_re("${ensure}", '^(present|absent)$')
+    validate_numeric($remind)
+    validate_string($email)
+
+    include ::hwraid
+
+    $package_require = $::osfamily ? {
+        'Debian' => Class['::hwraid::aptrepo'],
+        default  => undef,
+    }
+
+    Package {
+        ensure  => $ensure,
+        require => $package_require,
+    }
+    package { 'hwraid-tw-cli':       name => $::hwraid::params::twcli_package_name }
+    package { 'hwraid-3ware-status': name => $::hwraid::params::ware_status_package_name }
+
+    # On Debian the 3ware-statusd is configured using a file under 
+    # /etc/default.
+    if $::osfamily == 'Debian' {
+        file { 'hwraid-3ware-statusd':
+            ensure  => $ensure,
+            name    => '/etc/default/3ware-statusd',
+            content => template('hwraid/3ware-statusd.erb'),
+            owner   => $::os::params::adminuser,
+            group   => $::os::params::admingroup,
+            mode    => '0644',
+            notify  => Service['hwraid-3ware-statusd'],
+        }
+    }
+
+    $service_enable = $ensure ? {
+        'present' => true,
+        'absent'  => false,
+        default   => undef,
+    }
+
+    service { 'hwraid-3ware-statusd':
+        name    => $::hwraid::params::ware_status_service_name,
+        enable  => $service_enable,
+        require => Package['hwraid-3ware-status'],
+    }
+}
